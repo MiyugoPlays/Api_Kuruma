88672d2dbe5904dd66e144a1b2db9de9d1433aa7
MiyugoPlays <miyugoplays@gmail.com>
2024-05-14 09:51:45 -0300

    criação do ambiente pra criar a api

---
+++ script_db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE bdApiKuruma;
+
+use bdApiKuruma;
+
+CREATE TABLE carros
+(
+	codigo int primary key auto_increment,
+	modelo varchar(30),
+    placa varchar(7)
+);
+
+insert into carros (modelo, placa) values ('Fuscão Preto','ASD1234');
+insert into carros (modelo, placa) values ('Honda Fit','RRR4444');
+select * from carros;
---
+++ src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path: 'variables.env'})
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+const server = express()
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use('/api', routes)
+
+server.listen(process.env.PORT,()=>{
+    console.log(`Server running on http://localhost:${process.env.PORT}`) //Interpolação
+    //console.log("Servidor rodando em http://localhost:" + process.env.PORT) //Concatenação
+   
+})
