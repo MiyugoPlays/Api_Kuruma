88bacfcc7ed08a8bfe3514192b5c35bfb8f7fcb5
MiyugoPlays <miyugoplays@gmail.com>
2024-05-14 10:33:38 -0300

    Criação de uma rota, mostrando os dados do BD

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,1 +1,20 @@
 const CarroService = require ('../services/CarroService');
+
+module.exports = {
+    buscarTodos: async (req, res) =>{
+        let json = {error:'', result:[]}
+
+        let carros = await CarroService.buscarTodos();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                modelo: carros[i].modelo,
+                placa: carros[i].placa
+            })
+        }
+
+        res.json(json);
+    }
+
+}
--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,3 +1,15 @@
+const db = require('../db')
+
+db.query
+
 module.exports = {
-    
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('SELECT * FROM carros', (error, results)=>{
+                if(error) { rejeitado(error); return;}
+                aceito(results);
+            })
+        })
+        
+    }
 }
